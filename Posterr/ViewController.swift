0a50cd04a80b1b0c376b395afd34c7573b9597c3
Lucas Carvalho <empresarial.lucascarvalho@gmail.com>
2022-08-24 21:41:17 -0300

    initial project

---
+++ Posterr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posterr
+//
+//  Created by Lucas Carvalho on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
